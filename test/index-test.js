ec7cdea5b8bf110095abc927ccdfaad0ee6d8ddc
sensible-proc-7557 <vchan313@gmail.com>
2018-10-01 01:27:10 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -15,9 +15,7 @@ describe('Beatles Loops', function() {
         "He hated the sound of his own voice"
       ])).toEqual(["He was the last Beatle to learn to drive!!!", "He was never a vegetarian!!!", "He was a choir boy and boy scout!!!", "He hated the sound of his own voice!!!"]);
 
-      expect(johnLennonFacts([
-        
-      ])).toEqual(["foo!!!", "bar!!!"])
+      
     });
   });
 
