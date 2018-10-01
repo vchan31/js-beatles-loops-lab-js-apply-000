a22e21ef0ece50c1d4f4367828635ba1e48dccb6
sensible-proc-7557 <vchan313@gmail.com>
2018-10-01 01:23:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 // add solution here
 
-function theBeatlesPlay(musicians,instruments){
-  var whatTheyPlay = [];
-  for (let i = 0; i < musicians.length; i++){
-    for (let j = 0; i < instruments.length; j++) {
-      whatTheyPlay.push(musicians[i] + " plays " + instruments[j] ) 
-        return whatTheyPlay
-    } 
+function theBeatlesPlay(musicians, instruments){
+var whatTheyPlay = [];  
+
+for (let i = 0; i < musicians.length; i++) {
+  whatTheyPlay.push(musicians[i] + ' plays ' + instruments[i]);
+                    }
+return whatTheyPlay}
+
+
+
+function johnLennonFacts(){
+  var facts = ["He was the last Beatle to learn to drive","He was never a vegetarian","He was a choir boy and boy scout","He hated the sound of his own voice"];
+  var newFacts = [];
+  var counter = 0
+  while (counter < facts.length) {
+    newFacts.push(facts[counter] + "!!!")
+    counter++;
   }
+  return newFacts;
 }
-
-
-
-let musicians = ['John Lennon', 'Paul McCartney', 'George Harrison', 'Ringo Starr'];
-let instruments = ['guitar', 'bass', 'drums', 'vocals'];
-
