2a4aec37b7bd54b770d5add0a5eccbfac64b80ac
sensible-proc-7557 <vchan313@gmail.com>
2018-10-01 01:23:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // add solution here
+
+function theBeatlesPlay(musicians,instruments){
+  var whatTheyPlay = [];
+  for (let i = 0; i < musicians.length; i++){
+    for (let j = 0; i < instruments.length; j++) {
+      whatTheyPlay.push(musicians[i] + " plays " + instruments[j] ) 
+        return whatTheyPlay
+    } 
+  }
+}
+
+
+
+let musicians = ['John Lennon', 'Paul McCartney', 'George Harrison', 'Ringo Starr'];
+let instruments = ['guitar', 'bass', 'drums', 'vocals'];
+
