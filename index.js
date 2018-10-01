4ca5b2b0b15260f9a93a0e0ca2157b478118fa69
sensible-proc-7557 <vchan313@gmail.com>
2018-10-01 01:24:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,12 +11,4 @@ return whatTheyPlay}
 
 
 function johnLennonFacts(){
-  var facts = ["He was the last Beatle to learn to drive","He was never a vegetarian","He was a choir boy and boy scout","He hated the sound of his own voice"];
-  var newFacts = [];
-  var counter = 0
-  while (counter < facts.length) {
-    newFacts.push(facts[counter] + "!!!")
-    counter++;
-  }
-  return newFacts;
-}
+ 
